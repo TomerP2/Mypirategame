98794e09ccd8b541dfa5ac9d9a555e469a16ca98
TomerP2 <tomer.a.peled@gmail.com>
2022-10-20 16:53:04 +0200

    Changed movement system to use acceleration

--- MyPirateGame/Assets/PlayerMovement.cs
+++ MyPirateGame/Assets/PlayerMovement.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 1f;
+    public float acceleration = 0.005f;
+    public float maxSpeed = 10f;
+
+    public float rotationSpeed = 40f;
 
     public Rigidbody2D rb;
 
-    public float speed;
-    public float rotation;
+    private float currentSpeed = 0f;
+    private float rotation;
 
     // Update is called once per frame
     void Update(){
         // Input
-        rotation = Input.GetAxisRaw("Horizontal");
-        speed = Input.GetAxisRaw("Vertical");
+        rotation = Input.GetAxisRaw("Horizontal") * -1;
+        if (Input.GetAxisRaw("Vertical") > 0){
+            if (currentSpeed <= maxSpeed){
+                currentSpeed += Input.GetAxisRaw("Vertical") * acceleration;
+            }
+        }
+        if (Input.GetAxisRaw("Vertical")  < 0){
+            if (currentSpeed > 0){
+                currentSpeed += Input.GetAxisRaw("Vertical") * acceleration;
+            }
+        } 
+
+
     }
 
     void FixedUpdate() {
         // Movement
-        rb.rotation += rotation * rotationSpeed * -1 * Time.deltaTime;
-        transform.position += transform.up * speed * moveSpeed * Time.deltaTime;
+        rb.rotation += rotation * rotationSpeed * Time.deltaTime;
+        transform.position += transform.up * currentSpeed * Time.deltaTime;
     }
 }
